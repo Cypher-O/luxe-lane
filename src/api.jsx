c2f9594228748f602c4a16804e29fa45b5372f4c
Olumide Awodeji <awodejiolumidekolade@gmail.com>
2024-09-08 10:22:24 -0400

    Deployed on server & code cleanup

--- src/api.jsx
+++ src/api.jsx
@@ -1,33 +1,9 @@
-// // src/BASE.js
-// import axios from 'axios';
-
-// const BASE_URL = 'https://first-api-eb4r.onrender.com/api/products';
-
-// export const getProducts = () => axios.get(BASE_URL);
-// export const getProductById = (id) => axios.get(`${BASE_URL}/${id}`);
-// // export const searchProducts = (query) => axios.get(`${BASE_URL}/search?name=${query}`);
-// export const createProduct = (product) => axios.post(`${BASE_URL}/product`, product);
-// export const updateProduct = (id, product) => axios.put(`${BASE_URL}/${id}`, product);
-// export const deleteProduct = (id) => axios.delete(`${BASE_URL}/${id}`);
-
-// export const searchProducts = async (query) => {
-//     try {
-//         const response = await axios.get(`${BASE_URL}/products/search`, {
-//             params: { query }
-//         });
-//         return response.data;  // Ensure your API returns a `message` field when there are no results
-//     } catch (error) {
-//         console.error('Error searching products:', error);
-//         throw error; // Re-throw error to be handled in the calling function
-//     }
-// };
-
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'https://first-api-eb4r.onrender.com/api';
+const API_URL = 'https://luxe-lane-server-side.cybercraftsmen.tech/api';
 const PRODUCTS_URL = `${API_URL}/products`;
-const AUTH_URL = `${API_URL}/auth`; // Add the authentication base URL
+const AUTH_URL = `${API_URL}/auth`;
 
 // Products API
 export const getProducts = () => axios.get(PRODUCTS_URL);
@@ -41,10 +17,10 @@ export const searchProducts = async (name) => {
         const response = await axios.get(`${PRODUCTS_URL}/search`, {
             params: { name }
         });
-        return response.data;  // Ensure your API returns a `message` field when there are no results
+        return response.data;
     } catch (error) {
         console.error('Error searching products:', error);
-        throw error; // Re-throw error to be handled in the calling function
+        throw error; 
     }
 };
 
@@ -55,7 +31,7 @@ export const loginUser = async (email, password) => {
         return response.data; // Return the response data, usually containing a JWT token
     } catch (error) {
         console.error('Error logging in:', error);
-        throw error; // Re-throw error to be handled in the calling function
+        throw error; 
     }
 };
 
@@ -65,7 +41,7 @@ export const registerUser = async (username, email, password, confirmPassword) =
         return response.data; // Return the response data, usually a success message or user info
     } catch (error) {
         console.error('Error registering:', error);
-        throw error; // Re-throw error to be handled in the calling function
+        throw error; 
     }
 };
 
