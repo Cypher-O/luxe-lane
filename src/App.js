850e2cacf9d4e07a77171fd0e68f6f186ca18114
Olumide Awodeji <awodejiolumidekolade@gmail.com>
2024-09-09 06:12:05 -0400

    Code cleanup & improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Auth from './components/Auth'
+// import Dashboard from './components/Dashboard';
+// import PrivateRoute from './PrivateRoute';
+// import './App.css';
+
+// const App = () => {
+//     return (
+//       <div className="app-container">
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<Auth />} />
+//                 <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
+//             </Routes>
+//         </Router>
+//       </div>
+//     );
+// };
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './components/Auth'
+import Auth from './components/Auth';
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './PrivateRoute';
 import './App.css';
@@ -11,7 +33,14 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Auth />} />
-                <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
         </Router>
       </div>
--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,12 +1,28 @@
+// import React from 'react';
+// import { Navigate, useLocation } from 'react-router-dom';
+
+// const PrivateRoute = ({ element: Component, ...rest }) => {
+//     const isAuthenticated = !!localStorage.getItem('token');
+//     const location = useLocation();
+
+//     return isAuthenticated ? (
+//         <Component {...rest} />
+//     ) : (
+//         <Navigate to="/" state={{ from: location }} replace />
+//     );
+// };
+
+// export default PrivateRoute;
+
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = ({ element: Component, ...rest }) => {
+const PrivateRoute = ({ children }) => {
     const isAuthenticated = !!localStorage.getItem('token');
     const location = useLocation();
 
     return isAuthenticated ? (
-        <Component {...rest} />
+        children
     ) : (
         <Navigate to="/" state={{ from: location }} replace />
     );
